1d8739d9d42e9ab1514dbbb58283c3c9a47af379
Haryo Bimantoro <102338707+Haryo2021@users.noreply.github.com>
2023-09-10 13:35:55 +0700

    Update main.dart

--- main.dart
+++ main.dart
@@ -6,10 +6,11 @@ class Mahasiswa {
   Mahasiswa(this.nim, this.nama, this.tahunLahir);
 
   void perkenalan() {
-    print("Perkenalkan, nama saya $nama");
+    print("Perkenalkan, nama saya $nama, dengan NIM $nim, dan usia saya $tahunLahir tahun");
   }
 }
 
 void main() {
-  var mhs = Mahasiswa("H1D020068", "Abdalhaqq Muhammad Saih", 2002);
+  var mhs = Mahasiswa("H1D021071", "Haryo Bimantoro", 2000);
+  mhs.perkenalan();
 }
